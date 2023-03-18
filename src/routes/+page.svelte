d9d9b589627620daee3bea4fdb4205581fd4ba48
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-03-18 13:21:48 +0100

    make file longer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,7 +29,583 @@
   </div>
 
   <div
-    class="bg-gray-500 bg-opacity-15 shadow-lg p-2 mb-3"
+    class="bg-gray-500 bg-opacity-15 shadow-lg p-4 mb-3"
+    dir={rtl ? "rtl" : "ltr"}
+  >
+    <RightToLeftDependent />
+  </div>
+  <div class="mb-10">
+    <Button onclick={() => (rtl = !rtl)}>Toggle direction: {rtl ? "Right-to-left" : "Left-to-right"}</Button>
+  </div>
+
+  <Counter />
+
+  <Prose />
+
+  <div class="corner">Fixed</div>
+</div>
+
+<div class="p-1em my-2px mx-auto">
+  <h1
+    class="animate-bounce color-#ff3e00 dark:color-white uppercase text-5xl fw-100 my-2rem mx-auto"
+  >
+    UnoCSS + SvelteKit!
+  </h1>
+
+  <Logos />
+
+  <div class="my-15">
+    <div
+      class:bg-red-400={red}
+      class:bg-gray={!red}
+      class="text-white p-3 rounded mb-3">
+      My BG Color should change
+    </div>
+    <Button onclick={() => (red = !red)}>Change BG Color: {red ? "Gray" : "Red"}</Button>
+  </div>
+
+  <div
+    class="bg-gray-500 bg-opacity-15 shadow-lg p-4 mb-3"
+    dir={rtl ? "rtl" : "ltr"}
+  >
+    <RightToLeftDependent />
+  </div>
+  <div class="mb-10">
+    <Button onclick={() => (rtl = !rtl)}>Toggle direction: {rtl ? "Right-to-left" : "Left-to-right"}</Button>
+  </div>
+
+  <Counter />
+
+  <Prose />
+
+  <div class="corner">Fixed</div>
+</div>
+
+<div class="p-1em my-2px mx-auto">
+  <h1
+    class="animate-bounce color-#ff3e00 dark:color-white uppercase text-5xl fw-100 my-2rem mx-auto"
+  >
+    UnoCSS + SvelteKit!
+  </h1>
+
+  <Logos />
+
+  <div class="my-15">
+    <div
+      class:bg-red-400={red}
+      class:bg-gray={!red}
+      class="text-white p-3 rounded mb-3">
+      My BG Color should change
+    </div>
+    <Button onclick={() => (red = !red)}>Change BG Color: {red ? "Gray" : "Red"}</Button>
+  </div>
+
+  <div
+    class="bg-gray-500 bg-opacity-15 shadow-lg p-4 mb-3"
+    dir={rtl ? "rtl" : "ltr"}
+  >
+    <RightToLeftDependent />
+  </div>
+  <div class="mb-10">
+    <Button onclick={() => (rtl = !rtl)}>Toggle direction: {rtl ? "Right-to-left" : "Left-to-right"}</Button>
+  </div>
+
+  <Counter />
+
+  <Prose />
+
+  <div class="corner">Fixed</div>
+</div>
+
+<div class="p-1em my-2px mx-auto">
+  <h1
+    class="animate-bounce color-#ff3e00 dark:color-white uppercase text-5xl fw-100 my-2rem mx-auto"
+  >
+    UnoCSS + SvelteKit!
+  </h1>
+
+  <Logos />
+
+  <div class="my-15">
+    <div
+      class:bg-red-400={red}
+      class:bg-gray={!red}
+      class="text-white p-3 rounded mb-3">
+      My BG Color should change
+    </div>
+    <Button onclick={() => (red = !red)}>Change BG Color: {red ? "Gray" : "Red"}</Button>
+  </div>
+
+  <div
+    class="bg-gray-500 bg-opacity-15 shadow-md p-4 mb-3"
+    dir={rtl ? "rtl" : "ltr"}
+  >
+    <RightToLeftDependent />
+  </div>
+  <div class="mb-10">
+    <Button onclick={() => (rtl = !rtl)}>Toggle direction: {rtl ? "Right-to-left" : "Left-to-right"}</Button>
+  </div>
+
+  <Counter />
+
+  <Prose />
+
+  <div class="corner">Fixed</div>
+</div>
+
+<div class="p-1em my-2px mx-auto">
+  <h1
+    class="animate-bounce color-#ff3e00 dark:color-white uppercase text-5xl fw-100 my-2rem mx-auto"
+  >
+    UnoCSS + SvelteKit!
+  </h1>
+
+  <Logos />
+
+  <div class="my-15">
+    <div
+      class:bg-red-400={red}
+      class:bg-gray={!red}
+      class="text-white p-3 rounded mb-3">
+      My BG Color should change
+    </div>
+    <Button onclick={() => (red = !red)}>Change BG Color: {red ? "Gray" : "Red"}</Button>
+  </div>
+
+  <div
+    class="bg-gray-500 bg-opacity-15 shadow-lg p-4 mb-3"
+    dir={rtl ? "rtl" : "ltr"}
+  >
+    <RightToLeftDependent />
+  </div>
+  <div class="mb-10">
+    <Button onclick={() => (rtl = !rtl)}>Toggle direction: {rtl ? "Right-to-left" : "Left-to-right"}</Button>
+  </div>
+
+  <Counter />
+
+  <Prose />
+
+  <div class="corner">Fixed</div>
+</div>
+
+<div class="p-1em my-2px mx-auto">
+  <h1
+    class="animate-bounce color-#ff3e00 dark:color-white uppercase text-5xl fw-100 my-2rem mx-auto"
+  >
+    UnoCSS + SvelteKit!
+  </h1>
+
+  <Logos />
+
+  <div class="my-15">
+    <div
+      class:bg-red-400={red}
+      class:bg-gray={!red}
+      class="text-white p-3 rounded mb-3">
+      My BG Color should change
+    </div>
+    <Button onclick={() => (red = !red)}>Change BG Color: {red ? "Gray" : "Red"}</Button>
+  </div>
+
+  <div
+    class="bg-gray-500 bg-opacity-15 shadow-lg p-4 mb-3"
+    dir={rtl ? "rtl" : "ltr"}
+  >
+    <RightToLeftDependent />
+  </div>
+  <div class="mb-10">
+    <Button onclick={() => (rtl = !rtl)}>Toggle direction: {rtl ? "Right-to-left" : "Left-to-right"}</Button>
+  </div>
+
+  <Counter />
+
+  <Prose />
+
+  <div class="corner">Fixed</div>
+</div>
+
+<div class="p-1em my-2px mx-auto">
+  <h1
+    class="animate-bounce color-#ff3e00 dark:color-white uppercase text-5xl fw-100 my-2rem mx-auto"
+  >
+    UnoCSS + SvelteKit!
+  </h1>
+
+  <Logos />
+
+  <div class="my-15">
+    <div
+      class:bg-red-400={red}
+      class:bg-gray={!red}
+      class="text-white p-3 rounded mb-3">
+      My BG Color should change
+    </div>
+    <Button onclick={() => (red = !red)}>Change BG Color: {red ? "Gray" : "Red"}</Button>
+  </div>
+
+  <div
+    class="bg-gray-500 bg-opacity-15 shadow-lg p-4 mb-3"
+    dir={rtl ? "rtl" : "ltr"}
+  >
+    <RightToLeftDependent />
+  </div>
+  <div class="mb-10">
+    <Button onclick={() => (rtl = !rtl)}>Toggle direction: {rtl ? "Right-to-left" : "Left-to-right"}</Button>
+  </div>
+
+  <Counter />
+
+  <Prose />
+
+  <div class="corner">Fixed</div>
+</div>
+
+<div class="p-1em my-2px mx-auto">
+  <h1
+    class="animate-bounce color-#ff3e00 dark:color-white uppercase text-5xl fw-100 my-2rem mx-auto"
+  >
+    UnoCSS + SvelteKit!
+  </h1>
+
+  <Logos />
+
+  <div class="my-15">
+    <div
+      class:bg-red-400={red}
+      class:bg-gray={!red}
+      class="text-white p-3 rounded mb-3">
+      My BG Color should change
+    </div>
+    <Button onclick={() => (red = !red)}>Change BG Color: {red ? "Gray" : "Red"}</Button>
+  </div>
+
+  <div
+    class="bg-gray-500 bg-opacity-15 shadow-lg p-4 mb-3"
+    dir={rtl ? "rtl" : "ltr"}
+  >
+    <RightToLeftDependent />
+  </div>
+  <div class="mb-10">
+    <Button onclick={() => (rtl = !rtl)}>Toggle direction: {rtl ? "Right-to-left" : "Left-to-right"}</Button>
+  </div>
+
+  <Counter />
+
+  <Prose />
+
+  <div class="corner">Fixed</div>
+</div>
+
+<div class="p-1em my-2px mx-auto">
+  <h1
+    class="animate-bounce color-#ff3e00 dark:color-white uppercase text-5xl fw-100 my-2rem mx-auto"
+  >
+    UnoCSS + SvelteKit!
+  </h1>
+
+  <Logos />
+
+  <div class="my-15">
+    <div
+      class:bg-red-400={red}
+      class:bg-gray={!red}
+      class="text-white p-3 rounded mb-3">
+      My BG Color should change
+    </div>
+    <Button onclick={() => (red = !red)}>Change BG Color: {red ? "Gray" : "Red"}</Button>
+  </div>
+
+  <div
+    class="bg-gray-500 bg-opacity-15 shadow-lg p-4 mb-3"
+    dir={rtl ? "rtl" : "ltr"}
+  >
+    <RightToLeftDependent />
+  </div>
+  <div class="mb-10">
+    <Button onclick={() => (rtl = !rtl)}>Toggle direction: {rtl ? "Right-to-left" : "Left-to-right"}</Button>
+  </div>
+
+  <Counter />
+
+  <Prose />
+
+  <div class="corner">Fixed</div>
+</div>
+
+<div class="p-1em my-2px mx-auto">
+  <h1
+    class="animate-bounce color-#ff3e00 dark:color-white uppercase text-5xl fw-100 my-2rem mx-auto"
+  >
+    UnoCSS + SvelteKit!
+  </h1>
+
+  <Logos />
+
+  <div class="my-15">
+    <div
+      class:bg-red-400={red}
+      class:bg-gray={!red}
+      class="text-white p-3 rounded mb-3">
+      My BG Color should change
+    </div>
+    <Button onclick={() => (red = !red)}>Change BG Color: {red ? "Gray" : "Red"}</Button>
+  </div>
+
+  <div
+    class="bg-gray-500 bg-opacity-15 shadow-lg p-4 mb-3"
+    dir={rtl ? "rtl" : "ltr"}
+  >
+    <RightToLeftDependent />
+  </div>
+  <div class="mb-10">
+    <Button onclick={() => (rtl = !rtl)}>Toggle direction: {rtl ? "Right-to-left" : "Left-to-right"}</Button>
+  </div>
+
+  <Counter />
+
+  <Prose />
+
+  <div class="corner">Fixed</div>
+</div>
+
+<div class="p-1em my-2px mx-auto">
+  <h1
+    class="animate-bounce color-#ff3e00 dark:color-white uppercase text-5xl fw-100 my-2rem mx-auto"
+  >
+    UnoCSS + SvelteKit!
+  </h1>
+
+  <Logos />
+
+  <div class="my-15">
+    <div
+      class:bg-red-400={red}
+      class:bg-gray={!red}
+      class="text-white p-3 rounded mb-3">
+      My BG Color should change
+    </div>
+    <Button onclick={() => (red = !red)}>Change BG Color: {red ? "Gray" : "Red"}</Button>
+  </div>
+
+  <div
+    class="bg-gray-500 bg-opacity-15 shadow-lg p-4 mb-3"
+    dir={rtl ? "rtl" : "ltr"}
+  >
+    <RightToLeftDependent />
+  </div>
+  <div class="mb-10">
+    <Button onclick={() => (rtl = !rtl)}>Toggle direction: {rtl ? "Right-to-left" : "Left-to-right"}</Button>
+  </div>
+
+  <Counter />
+
+  <Prose />
+
+  <div class="corner">Fixed</div>
+</div>
+
+<div class="p-1em my-2px mx-auto">
+  <h1
+    class="animate-bounce color-#ff3e00 dark:color-white uppercase text-5xl fw-100 my-2rem mx-auto"
+  >
+    UnoCSS + SvelteKit!
+  </h1>
+
+  <Logos />
+
+  <div class="my-15">
+    <div
+      class:bg-red-400={red}
+      class:bg-gray={!red}
+      class="text-white p-3 rounded mb-3">
+      My BG Color should change
+    </div>
+    <Button onclick={() => (red = !red)}>Change BG Color: {red ? "Gray" : "Red"}</Button>
+  </div>
+
+  <div
+    class="bg-gray-500 bg-opacity-15 shadow-lg p-4 mb-3"
+    dir={rtl ? "rtl" : "ltr"}
+  >
+    <RightToLeftDependent />
+  </div>
+  <div class="mb-10">
+    <Button onclick={() => (rtl = !rtl)}>Toggle direction: {rtl ? "Right-to-left" : "Left-to-right"}</Button>
+  </div>
+
+  <Counter />
+
+  <Prose />
+
+  <div class="corner">Fixed</div>
+</div>
+
+<div class="p-1em my-2px mx-auto">
+  <h1
+    class="animate-bounce color-#ff3e00 dark:color-white uppercase text-5xl fw-100 my-2rem mx-auto"
+  >
+    UnoCSS + SvelteKit!
+  </h1>
+
+  <Logos />
+
+  <div class="my-15">
+    <div
+      class:bg-red-400={red}
+      class:bg-gray={!red}
+      class="text-white p-3 rounded mb-3">
+      My BG Color should change
+    </div>
+    <Button onclick={() => (red = !red)}>Change BG Color: {red ? "Gray" : "Red"}</Button>
+  </div>
+
+  <div
+    class="bg-gray-500 bg-opacity-15 shadow-lg p-4 mb-3"
+    dir={rtl ? "rtl" : "ltr"}
+  >
+    <RightToLeftDependent />
+  </div>
+  <div class="mb-10">
+    <Button onclick={() => (rtl = !rtl)}>Toggle direction: {rtl ? "Right-to-left" : "Left-to-right"}</Button>
+  </div>
+
+  <Counter />
+
+  <Prose />
+
+  <div class="corner">Fixed</div>
+</div>
+
+<div class="p-1em my-2px mx-auto">
+  <h1
+    class="animate-bounce color-#ff3e00 dark:color-white uppercase text-5xl fw-100 my-2rem mx-auto"
+  >
+    UnoCSS + SvelteKit!
+  </h1>
+
+  <Logos />
+
+  <div class="my-15">
+    <div
+      class:bg-red-400={red}
+      class:bg-gray={!red}
+      class="text-white p-3 rounded mb-3">
+      My BG Color should change
+    </div>
+    <Button onclick={() => (red = !red)}>Change BG Color: {red ? "Gray" : "Red"}</Button>
+  </div>
+
+  <div
+    class="bg-gray-500 bg-opacity-15 shadow-lg p-4 mb-3"
+    dir={rtl ? "rtl" : "ltr"}
+  >
+    <RightToLeftDependent />
+  </div>
+  <div class="mb-10">
+    <Button onclick={() => (rtl = !rtl)}>Toggle direction: {rtl ? "Right-to-left" : "Left-to-right"}</Button>
+  </div>
+
+  <Counter />
+
+  <Prose />
+
+  <div class="corner">Fixed</div>
+</div>
+
+<div class="p-1em my-2px mx-auto">
+  <h1
+    class="animate-bounce color-#ff3e00 dark:color-white uppercase text-5xl fw-100 my-2rem mx-auto"
+  >
+    UnoCSS + SvelteKit!
+  </h1>
+
+  <Logos />
+
+  <div class="my-15">
+    <div
+      class:bg-red-400={red}
+      class:bg-gray={!red}
+      class="text-white p-3 rounded mb-3">
+      My BG Color should change
+    </div>
+    <Button onclick={() => (red = !red)}>Change BG Color: {red ? "Gray" : "Red"}</Button>
+  </div>
+
+  <div
+    class="bg-gray-500 bg-opacity-15 shadow-lg p-4 mb-3"
+    dir={rtl ? "rtl" : "ltr"}
+  >
+    <RightToLeftDependent />
+  </div>
+  <div class="mb-10">
+    <Button onclick={() => (rtl = !rtl)}>Toggle direction: {rtl ? "Right-to-left" : "Left-to-right"}</Button>
+  </div>
+
+  <Counter />
+
+  <Prose />
+
+  <div class="corner">Fixed</div>
+</div>
+
+<div class="p-1em my-2px mx-auto">
+  <h1
+    class="animate-bounce color-#ff3e00 dark:color-white uppercase text-5xl fw-100 my-2rem mx-auto"
+  >
+    UnoCSS + SvelteKit!
+  </h1>
+
+  <Logos />
+
+  <div class="my-15">
+    <div
+      class:bg-red-400={red}
+      class:bg-gray={!red}
+      class="text-white p-3 rounded mb-3">
+      My BG Color should change
+    </div>
+    <Button onclick={() => (red = !red)}>Change BG Color: {red ? "Gray" : "Red"}</Button>
+  </div>
+
+  <div
+    class="bg-gray-500 bg-opacity-15 shadow-lg p-4 mb-3"
+    dir={rtl ? "rtl" : "ltr"}
+  >
+    <RightToLeftDependent />
+  </div>
+  <div class="mb-10">
+    <Button onclick={() => (rtl = !rtl)}>Toggle direction: {rtl ? "Right-to-left" : "Left-to-right"}</Button>
+  </div>
+
+  <Counter />
+
+  <Prose />
+
+  <div class="corner">Fixed</div>
+</div>
+
+<div class="p-1em my-2px mx-auto">
+  <h1
+    class="animate-bounce color-#ff3e00 dark:color-white uppercase text-5xl fw-100 my-2rem mx-auto"
+  >
+    UnoCSS + SvelteKit!
+  </h1>
+
+  <Logos />
+
+  <div class="my-15">
+    <div
+      class:bg-red-400={red}
+      class:bg-gray={!red}
+      class="text-white p-3 rounded mb-3">
+      My BG Color should change
+    </div>
+    <Button onclick={() => (red = !red)}>Change BG Color: {red ? "Gray" : "Red"}</Button>
+  </div>
+
+  <div
+    class="bg-gray-500 bg-opacity-15 shadow-lg p-4 mb-3"
     dir={rtl ? "rtl" : "ltr"}
   >
     <RightToLeftDependent />
