d35bc284376337357b06c4fda253c7b81d786f4d
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-03-18 13:10:22 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import "../app.css";
+  import Preflights from "./Preflights.svelte";
+  import DarkModeToggle from "$lib/DarkModeToggle.svelte";
+</script>
+
+<Preflights />
+
+<div
+  class="bg-white text-gray-900 dark:bg-gray-900 dark:text-white px-3 py-15 min-h-100vh transition"
+>
+  <main class="max-w-800px mx-auto text-center">
+    <slot />
+  </main>
+  <DarkModeToggle />
+</div>
+
